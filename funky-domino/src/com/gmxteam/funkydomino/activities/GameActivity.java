bf3b0dd82b5985996cfa7b469e02d3cc79e17b36
maxim.diouskine@gmail.com <maxim.diouskine@gmail.com@fa8fac95-30d9-4a24-001c-23bef12bbf44>
2012-02-02 17:07:33 +0000

    Structure du projet updaté

---
+++ funky-domino/src/com/gmxteam/funkydomino/activities/GameActivity.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gmxteam.funkydomino.activities;
+
+/**
+ *
+ * @author Usager
+ */
+public class GameActivity
+{
+    
+}
