93991c98b49e6ddb9679a9ebe4ce4b2f8deb3d93
maxim.diouskine@gmail.com <maxim.diouskine@gmail.com@fa8fac95-30d9-4a24-001c-23bef12bbf44>
2012-02-02 17:21:50 +0000

    Structure du projet updaté

---
+++ funky-domino/src/com/gmxteam/funkydomino/graphicals/components/Ground.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gmxteam.funkydomino.graphicals.components;
+
+/**
+ *
+ * @author Usager
+ */
+public class Ground
+{
+    
+}
