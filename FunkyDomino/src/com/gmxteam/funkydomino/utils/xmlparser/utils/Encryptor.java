8d34a5b4ef14ea3d6628366d07efdc68812ddc89
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2012-04-06 17:37:32 -0400

    Created an encryptor to make the xml files.

---
+++ FunkyDomino/src/com/gmxteam/funkydomino/utils/xmlparser/utils/Encryptor.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gmxteam.funkydomino.utils.xmlparser.utils;
+
+/**
+ *
+ * @author guillaume
+ */
+public class Encryptor {
+    public static void main(String[] args) {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
